e9f3e7acbe31ead14e5c904463057e25ccea9726
mi5ha6in <mi5ha6in@gmail.com>
2023-12-21 05:32:08 +0300

    Регистрирует BlogUserRepository в качестве провайдера

--- project/apps/user/src/app/blog-user/blog-user.module.ts
+++ project/apps/user/src/app/blog-user/blog-user.module.ts
@@ -1,4 +1,8 @@
 import { Module } from '@nestjs/common';
+import { BlogUserRepository } from './blog-user.repository';
 
-@Module({})
+@Module({
+  imports: [BlogUserRepository],
+  exports: [BlogUserRepository],
+})
 export class BlogUserModule {}
