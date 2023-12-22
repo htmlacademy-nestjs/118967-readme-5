75d9d41554e07d0163282f905f9eb7a2f0b0e979
mi5ha6in <mi5ha6in@gmail.com>
2023-12-22 05:28:12 +0300

    Добавляет метод регистрации в AuthenticationService

--- project/apps/user/src/app/authentication/authentication.service.ts
+++ project/apps/user/src/app/authentication/authentication.service.ts
@@ -1,7 +1,38 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
 import { BlogUserRepository } from '../blog-user/blog-user.repository';
+import { CreateUserDto } from './dto/create-user.dto';
+import { AuthUser } from '@project/shared-app-types';
+import dayjs from 'dayjs';
+import { AUTH_USER_EXISTS } from './authentication.constant';
+import { BlogUserEntity } from '../blog-user/blog-user.entity';
 
 @Injectable()
 export class AuthenticationService {
   constructor(private readonly blogUserRepository: BlogUserRepository) {}
+
+  public async register(dto: CreateUserDto) {
+    const { password, firstName, lastName, email, avatar } = dto;
+
+    const blogUser: AuthUser = {
+      id: '',
+      firstName,
+      lastName,
+      email,
+      avatar,
+      passwordHash: '',
+      registrationDate: dayjs().toDate(),
+      publicationCount: 0,
+      subscribersCount: 0,
+    };
+
+    const existUser = await this.blogUserRepository.findByEmail(email);
+
+    if (existUser) {
+      throw new ConflictException(AUTH_USER_EXISTS);
+    }
+
+    const userEntity = await new BlogUserEntity(blogUser).setPassword(password);
+
+    return this.blogUserRepository.save(userEntity);
+  }
 }
