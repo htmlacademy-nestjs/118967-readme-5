2583a9ec4f9680fe5b4b949cebe3f120148316cd
mi5ha6in <mi5ha6in@gmail.com>
2023-12-22 06:18:11 +0300

    Добавляет обработчик маршрута /register

--- project/apps/user/src/app/authentication/authentication.controller.ts
+++ project/apps/user/src/app/authentication/authentication.controller.ts
@@ -1,7 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { AuthenticationService } from './authentication.service';
+import { fillDto } from '@project/shared-helpers';
+import { UserRdo } from './rdo/user.rdo';
+import { CreateUserDto } from './dto/create-user.dto';
 
 @Controller('auth')
 export class AuthenticationController {
   constructor(private readonly authService: AuthenticationService) {}
+
+  @Post('register')
+  public async create(@Body() dto: CreateUserDto) {
+    const newUser = await this.authService.register(dto);
+    return fillDto(UserRdo, newUser.toPOJO());
+  }
 }
