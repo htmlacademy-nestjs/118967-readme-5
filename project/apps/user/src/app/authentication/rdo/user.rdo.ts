5732482b9e7ec2ddaffba948a79ef24fc9af37f1
mi5ha6in <mi5ha6in@gmail.com>
2023-12-22 07:58:53 +0300

    Добавляет документацию сваггера

--- project/apps/user/src/app/authentication/rdo/user.rdo.ts
+++ project/apps/user/src/app/authentication/rdo/user.rdo.ts
@@ -1,27 +1,60 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { Expose } from 'class-transformer';
 
 export class UserRdo {
+  @ApiProperty({
+    description: 'The uniq user ID',
+    example: '13',
+  })
   @Expose()
   public id: string;
 
+  @ApiProperty({
+    description: 'User email',
+    example: 'user@user.local',
+  })
   @Expose()
   public email: string;
 
+  @ApiProperty({
+    description: 'User first name',
+    example: 'Keks',
+  })
   @Expose()
   public firstName: string;
 
+  @ApiProperty({
+    description: 'User last name',
+    example: 'Keks',
+  })
   @Expose()
   public lastName: string;
 
+  @ApiProperty({
+    description: 'User avatar path',
+    example: '/images/user.png',
+  })
   @Expose()
   public avatar: string;
 
+  @ApiProperty({
+    description: 'User registration date (ISO format)',
+    example: '1981-03-12',
+  })
   @Expose()
   public registrationDate: string;
 
+  @ApiProperty({
+    description: 'User registration publication count',
+    example: 10,
+  })
   @Expose()
   public publicationCount: number;
 
+  @ApiProperty({
+    description: 'User registration subscribers count',
+    example: 10,
+  })
   @Expose()
-  public subscribersCount: never;
+  public subscribersCount: number;
 }
